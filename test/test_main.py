931e693c8e62edbd8fd4748d245af42a041b6223
Sunanda Garg <sgarg@sunandas-air.speedport.ip>
2024-07-24 11:28:14 +0200

    setting up tests for POST endpoint /predict

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,12 @@
+import pytest
+from numpy import ndarray
 from fastapi.testclient import TestClient
 
 from application.main import app
 
 client = TestClient(app)
 
-# Testing get method say_hello()
+# Testing the GET / endpoint
 def test_say_hello():
     r = client.get("/")
     # Testing successfull get call (200 OK)
@@ -14,6 +16,35 @@ def test_say_hello():
     # Testing unsuccessfull get call (404 not found)
     r = client.get("/{some_param}")
     assert r.status_code == 404
+
+# Testing the POST /predict endpoint
+@pytest.mark.asyncio
+async def test_predict():
+    # Testing unsuccessfull post call (400 bad request) due to wrong input values
+    csv_content = """feature1,feature2,feature3
+    1,2,3
+    4,5,6
+    7,8,9
+    """
+    files = {'file': ('test.csv', csv_content, 'text/csv')}
+    response = client.post("/predict", files=files)
+    assert response.status_code == 400
+
+    # Testing successfull post call (200 ok)
+    csv_content = """workclass_?,workclass_Federal-gov,workclass_Local-gov,workclass_Never-worked,workclass_Private,workclass_Self-emp-inc,workclass_Self-emp-not-inc,workclass_State-gov,workclass_Without-pay,education_10th,education_11th,education_12th,education_1st-4th,education_5th-6th,education_7th-8th,education_9th,education_Assoc-acdm,education_Assoc-voc,education_Bachelors,education_Doctorate,education_HS-grad,education_Masters,education_Preschool,education_Prof-school,education_Some-college,marital-status_Divorced,marital-status_Married-AF-spouse,marital-status_Married-civ-spouse,marital-status_Married-spouse-absent,marital-status_Never-married,marital-status_Separated,marital-status_Widowed,occupation_?,occupation_Adm-clerical,occupation_Armed-Forces,occupation_Craft-repair,occupation_Exec-managerial,occupation_Farming-fishing,occupation_Handlers-cleaners,occupation_Machine-op-inspct,occupation_Other-service,occupation_Priv-house-serv,occupation_Prof-specialty,occupation_Protective-serv,occupation_Sales,occupation_Tech-support,occupation_Transport-moving,relationship_Husband,relationship_Not-in-family,relationship_Other-relative,relationship_Own-child,relationship_Unmarried,relationship_Wife,race_Amer-Indian-Eskimo,race_Asian-Pac-Islander,race_Black,race_Other,race_White,sex_Female,sex_Male,native-country_?,native-country_Cambodia,native-country_Canada,native-country_China,native-country_Columbia,native-country_Cuba,native-country_Dominican-Republic,native-country_Ecuador,native-country_El-Salvador,native-country_England,native-country_France,native-country_Germany,native-country_Greece,native-country_Guatemala,native-country_Haiti,native-country_Holand-Netherlands,native-country_Honduras,native-country_Hong,native-country_Hungary,native-country_India,native-country_Iran,native-country_Ireland,native-country_Italy,native-country_Jamaica,native-country_Japan,native-country_Laos,native-country_Mexico,native-country_Nicaragua,native-country_Outlying-US(Guam-USVI-etc),native-country_Peru,native-country_Philippines,native-country_Poland,native-country_Portugal,native-country_Puerto-Rico,native-country_Scotland,native-country_South,native-country_Taiwan,native-country_Thailand,native-country_Trinadad&Tobago,native-country_United-States,native-country_Vietnam,native-country_Yugoslavia,age,fnlgt,education-num,capital-gain,capital-loss,hours-per-week
+    0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,21,478457,10,0,0,12
+    """
+    files = {'file': ('test.csv', csv_content, 'text/csv')}
+    response = client.post("/predict", files=files)
+    assert response.status_code == 200
+    # Testing successfull post call return types
+    json_response = response.json()
+    assert "prediction" in json_response
+    predictions = json_response["prediction"]
+    assert isinstance(predictions, list)
+    assert all(isinstance(p, str) for p in predictions)
+    # Testing successfull post call return values
+    assert predictions[0] == "<=50K"
 
 
 
